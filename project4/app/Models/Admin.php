457661b70b40dc66c68a15eea2339602093cab03
ikbalhossainn <ikbalhossain125@gmail.com>
2024-01-09 10:17:10 +0600

    updated

---
+++ project4/app/Models/Admin.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable; // copy from User.php
+use Illuminate\Notifications\Notifiable; // copy from User.php
+use Laravel\Sanctum\HasApiTokens; // copy from User.php
+
+
+class Admin extends Model
+{
+    // copy from User.php
+      /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+}
