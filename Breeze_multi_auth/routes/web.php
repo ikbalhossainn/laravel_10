e8946edf90b28ee14ff6a1fb889e857022b5f7b7
ikbalhossainn <ikbalhossain125@gmail.com>
2024-01-30 11:39:50 +0600

    updated

--- Breeze_multi_auth/routes/web.php
+++ Breeze_multi_auth/routes/web.php
@@ -40,4 +40,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-// require _DIR_ . '/auth.php';
+ require _DIR_ . '/auth.php';
