3f59fdde22d2b225fea1c6571ff0fa5c15e38a77
ikbalhossainn <ikbalhossain125@gmail.com>
2024-02-19 10:59:54 +0600

    fkg;flkgfgff

--- laravelbreeze-inertia-react/routes/web.php
+++ laravelbreeze-inertia-react/routes/web.php
@@ -17,11 +17,11 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
+    return Inertia::render('Home', [
+        // 'canLogin' => Route::has('login'),
+        // 'canRegister' => Route::has('register'),
+        // 'laravelVersion' => Application::VERSION,
+        // 'phpVersion' => PHP_VERSION,
     ]);
 });
 
